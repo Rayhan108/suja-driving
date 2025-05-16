b0ea1d3049c2ce4fe51e290996b24a9ca2a9ea4d
unknown <raihanshorker56@gmail.com>
2025-05-16 20:07:02 +0600

    fix error

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,15 +1,7 @@
-
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
-
-
-
-
-
+import { createBrowserRouter } from "react-router-dom";
 
 import Main from "../layout/Main";
-import Signin from "../pages/SignIn/SignIn";
+import Signin from "../pages/SignIn/Signin";
 import Verify from "../pages/Verify/Verify";
 import ForgotPass from "../pages/ForgotPass/ForgotPass";
 import SetPass from "../pages/SetPass/SetPass";
@@ -32,113 +24,109 @@ import Terms from "../pages/Terms/Terms";
 import Privacy from "../pages/Privacy/Privacy";
 import PassReset from "../pages/PassReset/PassReset";
 
-  
-  const router = createBrowserRouter([
-    {
-      path: "/sign-in",
-      element: <Signin />,
-    },
-    {
-      path: "/verify",
-      element: <Verify />,
-    },
-    {
-      path: "/passReset",
-      element: <PassReset />,
-    },
-    {
-      path: "/forget-password",
-      element: <ForgotPass/>,
-    },
-    {
-      path: "/setPass",
-      element: <SetPass/>,
-    },
-    {
+const router = createBrowserRouter([
+  {
+    path: "/sign-in",
+    element: <Signin />,
+  },
+  {
+    path: "/verify",
+    element: <Verify />,
+  },
+  {
+    path: "/passReset",
+    element: <PassReset />,
+  },
+  {
+    path: "/forget-password",
+    element: <ForgotPass />,
+  },
+  {
+    path: "/setPass",
+    element: <SetPass />,
+  },
+  {
+    path: "/",
+    element: <Main></Main>,
+    // errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
         path: "/",
-        element: <Main></Main>,
-        // errorElement: <ErrorPage></ErrorPage>,
-        children: [
-          {
-            path: "/",
-            element: <DashboardPage/>,
-          },
-          {
-            path: "/userManagement",
-            element: <UserManagement/>,
-          },
-          {
-            path: "/theoryManagement",
-            element: <TheoryManagement/>,
-          },
-          {
-            path: "/theoryManagement/category",
-            element: <TheoryManagement/>,
-          },
-          {
-            path: "/theoryManagement/topic",
-            element: <TheoryManagementTopic/>,
-          },
-          {
-            path: "/theoryManagement/question",
-            element: <QuestionManagement/>,
-          },
-          {
-            path: "/adiTheoryManagement",
-            element: <AdiTheoryManagement/>,
-          },
-          {
-            path: "/adiTheoryManagement/category",
-            element: <AdiTheoryManagement/>,
-          },
-          {
-            path: "/adiTheoryManagement/topic",
-            element: <AdiTheoryManagementTopic/>,
-          },
-          {
-            path: "/adiTheoryManagement/question",
-            element: <AdiQuestionManagement/>,
-          },
-          {
-            path: "/hazard",
-            element: <HazardPage/>,
-          },
-          {
-            path: "/highway",
-            element: <HighwayPage/>,
-          },
-          {
-            path: "/testScore",
-            element: <TestScore/>,
-          },
-          {
-            path: "/subscription",
-            element: <Subscription/>,
-          },
-          {
-            path: "/feedback",
-            element: <Feedback/>
-          },
-          {
-            path: "/analytics",
-            element: <Analytics/>
-          },
-          {
-            path: "/setting/updateProfile",
-            element: <UpdateProfile/>
-          },
-          {
-            path: "/setting/terms",
-            element: <Terms/>
-          },
-          {
-            path: "/setting/privacy",
-            element: <Privacy/>
-          },
-        
-         
-        ]
-    },
-
-  ]);
-  export default router;
+        element: <DashboardPage />,
+      },
+      {
+        path: "/userManagement",
+        element: <UserManagement />,
+      },
+      {
+        path: "/theoryManagement",
+        element: <TheoryManagement />,
+      },
+      {
+        path: "/theoryManagement/category",
+        element: <TheoryManagement />,
+      },
+      {
+        path: "/theoryManagement/topic",
+        element: <TheoryManagementTopic />,
+      },
+      {
+        path: "/theoryManagement/question",
+        element: <QuestionManagement />,
+      },
+      {
+        path: "/adiTheoryManagement",
+        element: <AdiTheoryManagement />,
+      },
+      {
+        path: "/adiTheoryManagement/category",
+        element: <AdiTheoryManagement />,
+      },
+      {
+        path: "/adiTheoryManagement/topic",
+        element: <AdiTheoryManagementTopic />,
+      },
+      {
+        path: "/adiTheoryManagement/question",
+        element: <AdiQuestionManagement />,
+      },
+      {
+        path: "/hazard",
+        element: <HazardPage />,
+      },
+      {
+        path: "/highway",
+        element: <HighwayPage />,
+      },
+      {
+        path: "/testScore",
+        element: <TestScore />,
+      },
+      {
+        path: "/subscription",
+        element: <Subscription />,
+      },
+      {
+        path: "/feedback",
+        element: <Feedback />,
+      },
+      {
+        path: "/analytics",
+        element: <Analytics />,
+      },
+      {
+        path: "/setting/updateProfile",
+        element: <UpdateProfile />,
+      },
+      {
+        path: "/setting/terms",
+        element: <Terms />,
+      },
+      {
+        path: "/setting/privacy",
+        element: <Privacy />,
+      },
+    ],
+  },
+]);
+export default router;

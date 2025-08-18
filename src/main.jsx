fc045ca1d710ed64c11ead662456ce61a5e3321c
rayhan108 <raihanshorker56@gmail.com>
2025-08-18 15:24:10 +0600

    setup redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,19 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+           <Provider store={store}>
+       <PersistGate loading={null} persistor={persistor}>
+
     <ToastContainer/>
     <RouterProvider router={router} />
+       </PersistGate>
+       </Provider>
   </StrictMode>
 );

c21d2a421aded8caf5ad54ecab5036aba4c54f64
rayhan108 <raihanshorker56@gmail.com>
2025-08-25 17:39:02 +0600

    change base url

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://10.10.20.9:5055",
+    baseUrl: "https://sujadriving-backend.onrender.com",
     // credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState()).auth.token;

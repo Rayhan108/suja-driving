0a09df958dde96b41bc5f0e00bc085b150ffe0ac
rayhan108 <raihanshorker56@gmail.com>
2025-09-05 13:35:51 +0600

    complete solve issues

--- src/pages/Analytics/Analytics.jsx
+++ src/pages/Analytics/Analytics.jsx
@@ -75,7 +75,7 @@ const Analytics = () => {
     return (
         <div>
                     <div className="flex  items-center gap-5 mt-3 mb-12">
-                      <SlArrowLeft className="w-5 h-5 text-right text-[#3564d3]" />
+        
                       <p className="text-[#3564d3] font-title text-3xl font-bold">
                Analytics & Reports
                       </p>
--- src/pages/TestScore/TestScore.jsx
+++ src/pages/TestScore/TestScore.jsx
@@ -17,7 +17,7 @@ const TestScore = () => {
   return (
     <div>
       <div className="flex  items-center gap-5 mt-3 mb-12">
-        <SlArrowLeft className="w-5 h-5 text-right text-[#3564d3]" />
+       
         <p className="text-[#3564d3] font-title text-3xl font-bold">
           Test & Scoring Settings
         </p>
